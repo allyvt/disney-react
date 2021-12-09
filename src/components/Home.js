5a83654a00f8bf240ecf0469891661fa39ba1043
Ally <allyvu49@gmail.com>
2021-12-09 16:50:05 -0500

    View & Movies Done

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
-import ImgSlider from './ImgSlider'
+import ImgSlider from './ImgSlider';
+import Viewers from './Viewers';
+import Movies from './Movies';
 
 function Home() {
     return (
         <Container>
            <ImgSlider/>
-            {/* <Viewers/> */}
-            {/* <Movies/> */} 
+           <Viewers/>
+           <Movies/>
+           
         </Container>
     )
 }
@@ -17,7 +20,7 @@ export default Home
 
 const Container = styled.main`
     min-height: calc(100vh - 70px);
-    padding: 0 calc(3.5vh + 5px);
+    padding: 0 calc(3.5vw + 5px);
     position: relative;
     overflow-x: hidden;
 
@@ -29,6 +32,7 @@ const Container = styled.main`
         right: 0;
         top: 0;
         bottom: 0;
+        z-index:-1;
     }
 `
 
